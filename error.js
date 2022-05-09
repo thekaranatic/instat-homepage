3ae5abd7557176d8faf7e13d93382c0c38295c38
Karan Kakati <karanpkofficial@gmail.com>
2022-05-09 19:29:46 +0530

    Update error.js

--- error.js
+++ error.js
@@ -1,6 +1,19 @@
 function alert_No_access_to_mobile(){
-    if(isMobileDevice)
-        alert("dont use");
-    else
-        window.location.href = 'app.html';
+    /* Storing user's device details in a variable*/
+    let details = navigator.userAgent;
+  
+    /* Creating a regular expression 
+    containing some mobile devices keywords 
+    to search it in details string*/
+    let regexp = /android|iphone|kindle|ipad/i;
+
+    /* Using test() method to search regexp in details
+    it returns boolean value*/
+    let isMobileDevice = regexp.test(details);
+
+    if (isMobileDevice) {
+        window.location.href = 'error.html';
+    } else {
+        alert("You are using Desktop");
+    }
 }
