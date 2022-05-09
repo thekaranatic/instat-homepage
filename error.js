3f63992d88f2c0d3189d724a53402826d9f54359
Karan Kakati <karanpkofficial@gmail.com>
2022-05-09 19:24:40 +0530

    Update error.js

--- error.js
+++ error.js
@@ -1,6 +1,6 @@
 function alert_No_access_to_mobile(){
     if(isMobileDevice)
-        window.location.href = 'error.html';
+        alert("dont use");
     else
         window.location.href = 'app.html';
 }
